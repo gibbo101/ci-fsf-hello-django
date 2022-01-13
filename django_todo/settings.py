e4fe840514c86924aa2580cc2a643bc987500a74
Luke Gibson <lukegibson101@gmail.com>
2022-01-13 17:33:40 +0000

    fix code error

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-o(8bhatdizfu_8hkzckcf(bvrw8ao32sus57o07idtr^0*fzy+'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-o(8bhatdizfu_8hkzckcf(bvrw8ao32sus57o07idtr^0*fzy+')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
